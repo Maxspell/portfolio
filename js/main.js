3133ea888efd22f706390d08a5ea4ba00d50768d
Maxspell <m.stizhko@gmail.com>
2025-08-26 01:06:01 +0300

    Add scroll to top

--- js/main.js
+++ js/main.js
@@ -1,10 +1,57 @@
-// Service section - Modal
+// header effect scroll
+const header = document.querySelector('.header');
+const scrollHeader = () => {
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+window.addEventListener('scroll', scrollHeader);
 
+// Navigation menu item active
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section[id]');
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id');
+        let navLink = document.querySelector('.nav-menu a[href*=' + sectionId + ']');
+
+        if (navLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add('active');
+            } else {
+                navLink.classList.remove('active');
+            }
+        }
+    });
+})
+
+// Scroll to top button
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollBtn = document.querySelector('.scroll-to-top');
+
+    if (!scrollBtn) return;
+
+    const toggleScrollBtn = () => {
+        scrollBtn.classList.toggle('active', window.scrollY > 500);
+    };
+
+    const handleScrollTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    window.addEventListener('scroll', toggleScrollBtn);
+    scrollBtn.addEventListener('click', handleScrollTop);
+});
+
+// Service section - Modal
 const serviceModal = document.querySelectorAll('.service-modal');
 const learnMoreBtn = document.querySelectorAll('.learn-more-btn');
 const modalCloseBtn = document.querySelectorAll('.modal-close-btn');
 
-const modal = function(modalClick) {
+const modal = function (modalClick) {
     serviceModal[modalClick].classList.add('active');
 }
 
@@ -21,12 +68,11 @@ modalCloseBtn.forEach((btn, i) => {
 });
 
 // Portfolio section - Modal
-
 const portfolioModals = document.querySelectorAll('.portfolio-modal');
 const imgCard = document.querySelectorAll('.img-card');
 const portfolioCloseBtn = document.querySelectorAll('.portfolio-close-btn');
 
-const portfolioModal = function(modalClick) {
+const portfolioModal = function (modalClick) {
     portfolioModals[modalClick].classList.add('active');
 }
 
@@ -43,7 +89,6 @@ portfolioCloseBtn.forEach((btn, i) => {
 })
 
 // Our Clients section - Swiper
-
 const ourClientsSwiper = new Swiper('.our-clients-swiper', {
     slidesPerView: 1,
     spaceBetween: 30,
